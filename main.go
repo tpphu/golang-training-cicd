2da92102da49075109d650713c2aadeb0e05172f
Tran Phong Phu <vietean@gmail.com>
2019-04-15 09:53:21 +0700

    - Update

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello from NordicCoder")
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
