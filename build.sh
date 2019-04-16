0c75084902730ee58bd05a241e4cc64018396027
Tran Phong Phu <vietean@gmail.com>
2019-04-16 21:38:57 +0700

    * Update

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 cd /home/thedevband/golang-training-cicd
+git reset --hard
+git pull
+chmod a+x build.sh
 /usr/sbin/lsof -t -i tcp:8080 -s tcp:listen | xargs kill
 go build -o bin/golang-training-cicd -v .
 nohup ./bin/golang-training-cicd >> ./bin/log.log 2>&1 &
